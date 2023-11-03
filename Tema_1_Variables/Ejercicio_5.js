7a7b2c54f85b77efedc11d887f520acef099ec6c
JackDev21 <jose.a.c.lopez@gmail.com>
2023-11-03 19:39:02 +0100

    Ejercicio5

--- Tema_1_Variables/Ejercicio_5.js
+++ Tema_1_Variables/Ejercicio_5.js
@@ -2,7 +2,9 @@
 
 const info = ["36", "Canto", "Jose"];
 
-for (let i = 0; i < info.length; ++i); //
+for (let i = 0; i < info.length; ++i); 
+// Iniciamos en 0 y vamos hasta la longitud del array, en este caso 3, por lo tanto el bucle se repetirá 3 veces, incrementando el valor de "i" de 1 en 1, en cada iteración.
+
 console.log(info[0])
 console.log(info[1])
 console.log(info[2])
