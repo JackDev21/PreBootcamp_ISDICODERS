3ea0bf04c8a92a6794f105d859da994d72208f2b
JackDev21 <jose.a.c.lopez@gmail.com>
2023-11-04 11:56:52 +0100

    Ejercicio7

--- Tema_1_Variables/Ejercicio_07.js
+++ Tema_1_Variables/Ejercicio_07.js
@@ -1,11 +1,14 @@
 // Crea una array de 5 numeros, y recorrela, mostrando además un mensaje cuando, esté a la mitad, muestre un mensaje "We are in the middle of loop".
 
 const listnum = [1, 2, 3, 4, 5];
-const mitad = Math.floor(listnum.length / 2); 
+const mitad = Math.floor(listnum.length / 2);   // Utilizamos Math.floor para redondear hacia abajo al entero mas cercano. 
+                                                //Y dividimos listnum entre 2 para encontrar la mitad.
 
+//Bucle for que recorre una lista de números. El bucle comienza en 0 y continúa hasta que el valor de 'num' sea menor que la longitud de la lista 'listnum'. 
+//En cada iteración, 'num' se incrementa en 1.
 for (let num = 0; num < listnum.length; num++) { 
-    if (num === mitad) {
-    console.log("We are in the middle of loop.");
+    if (num === mitad) { // Utilizamos if para comparar si el valor de "num" es igual a "mitad" entonces estaremos en el medio del bucle.
+    console.log("We are in the middle of loop."); // e imprimimos que estamos en el medio del bucle for.
     }
-    console.log(listnum[num])
-}
+    console.log(listnum[num]) // Aqúi imprimimos la "listnum" con los numeros recorridos.
+} 
