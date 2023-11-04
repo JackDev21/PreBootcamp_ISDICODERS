5e60fcf468a19f8165b5b9941fdce19e502e0146
JackDev21 <jose.a.c.lopez@gmail.com>
2023-11-04 16:02:35 +0100

    Ejercicio 5 Tema 2

---
+++ Tema_2_Metodos_y_Funciones/Ejercicio_05a.js
@@ -0,0 +1,10 @@
+// Imprime por consola tu apellido transfórmandolo a MAYÚSCULAS.
+
+
+const nombre = "Jack"
+const apellido = "Cantó"
+
+const nombrecompleto = `${nombre} ${apellido}`
+
+const apellidoUppercase = apellido.toUpperCase() // Utilizamos toUpperCase() para transformar el texto a mayúsculas.
+console.log (`Mi apellido es, ${apellidoUppercase}`)
