f9ed969b10b9c1ea9353d92157f5fcd016855eda
JackDev21 <jose.a.c.lopez@gmail.com>
2023-11-10 20:22:21 +0100

    Update

--- Tema_4_Functions/Ejercicio_8_Functions.js
+++ Tema_4_Functions/Ejercicio_8_Functions.js
@@ -2,10 +2,10 @@
 
 // Función que genera un número aleatorio entre 1900 y 2050.
 const obtenerNumeroAleatorio = () => {
-     const numeroMaximo = 2051; // Establece el valor máximo del número aleatorio.
-     const numeroMinimo = 1900; // Establece el valor mínimo del número aleatorio.
-     // Genera un número aleatorio utilizando el rango establecido y lo redondea hacia abajo.
-     return Math.floor(Math.random() * (numeroMaximo - numeroMinimo)) + numeroMinimo;
+    const numeroMaximo = 2051; // Establece el valor máximo del número aleatorio.
+    const numeroMinimo = 1900; // Establece el valor mínimo del número aleatorio.
+    // Genera un número aleatorio utilizando el rango establecido y lo redondea hacia abajo.
+    return Math.floor(Math.random() * (numeroMaximo - numeroMinimo)) + numeroMinimo;
 }
 
 // Función que devuelve el nombre proporcionado como argumento.
@@ -15,9 +15,9 @@ const obtenerMiNombre = (miNombre) => {
 
 // Función que calcula la edad basándose en el año de nacimiento proporcionado como argumento.
 const calcularMiEdad = (anioNacimiento) => {
-     const anioActual = new Date().getFullYear(); // Obtiene el año actual.
-     let edad = anioActual - anioNacimiento; // Calcula la diferencia entre el año actual y el año de nacimiento.
-     if (edad < 0) { // Si la edad calculada es menor a 0, se establece como 0.
+    const anioActual = new Date().getFullYear(); // Obtiene el año actual.
+    let edad = anioActual - anioNacimiento; // Calcula la diferencia entre el año actual y el año de nacimiento.
+    if (edad < 0) { // Si la edad calculada es menor a 0, se establece como 0.
         edad = 0;
     }
     return edad; // Devuelve la edad calculada.
---
+++ Tema_5_Ejercicios/Ejercicio_01.js
@@ -0,0 +1,3 @@
+// Escribe una función que declare un array con los números del 1 al 9 y muestre por pantalla los números unidos por parejas (1-2, 2-3, 3-4, ...) 
+// multiplicados por 2. Puedes utilizar los métodos push(), join(), y map(). Además, la función debería aceptar como argumentos el array a tratar, 
+// el número a multiplicar, y el número de parejas a mostrar.
