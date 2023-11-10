4c2f0f3fa1ffea01b5326cfc0294cc355a01168c
JackDev21 <jose.a.c.lopez@gmail.com>
2023-11-10 19:57:13 +0100

    Ejercicio 7 Tema 4 Funciones

--- pruebas.js
+++ pruebas.js
@@ -1,6 +1,29 @@
+const getRandomNumber = () => {
+  const maxNumber = 2050;
+  const minNumber = 1900;
+
+  return Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
+};
+
 const getMyName = (myName) => {
-  console.log(myName); //output: "hello myName"
+  return `myName, `;
+};
+
+
+const calculateMyAge = (birthOfYear) => {
+  const currentYear = new Date().getFullYear();
+  const age = currentYear - birthOfYear;
+
+  return `I'm ${age} years old`;
+};
+
+const getMyPersonalData = (nameData) => {
+  const name = getMyName(nameData);
+  const randomYear = getRandomNumber();
+  const age = calculateMyAge(randomYear);
+  return `${name} ${age}`;
 };
 
 const name = "Jon";
-console.log(`hello my name is ${getMyName(name)}`);
+
+console.log(getMyPersonalData(name));
