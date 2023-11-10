b85536cc3052e162a8d539f84edacba9a9fe5295
JackDev21 <jose.a.c.lopez@gmail.com>
2023-11-10 18:57:32 +0100

    Ejercicio 6 Tema 4 Functions

--- Tema_4_Functions/Ejercicio_5_Functions.js
+++ Tema_4_Functions/Ejercicio_5_Functions.js
@@ -1,11 +1,16 @@
 // Ahora declara otra función que calcule tu edad respecto tu año de nacimiento.
 
-const calcEdad = (añoNacimiento)  =>{
+const calcEdad = (añoNacimiento)  => {
+    // new Date() crea una nueva instancia del objeto Date, que representa la fecha y hora actual.
+    //.getFullYear() es un método del objeto Date que devuelve el año actual como un número de cuatro dígitos.
     const añoActual = new Date().getFullYear();
-    const edad = añoActual - añoNacimiento
-
-    console.log(`Tengo ${edad} años.`)
     
+    // Calcula la edad restando el año de nacimiento al año actual
+    const edad = añoActual - añoNacimiento;
+
+    // Imprime un mensaje en la consola mostrando la edad calculada
+    console.log(`Tengo ${edad} años.`);
 }
 
-calcEdad(1987)
+// Llama a la función calcEdad pasando 1987 como argumento
+calcEdad(1987);
