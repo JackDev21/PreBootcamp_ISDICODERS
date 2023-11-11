fa6fbef49d3e1af4c543ddd5a1ee26eabf8d5457
JackDev21 <jose.a.c.lopez@gmail.com>
2023-11-11 16:30:32 +0100

    Metodos, fibonacci, airline

---
+++ Coders Airlines!/Airline.js
@@ -0,0 +1,9 @@
+/*Programa una primera versión (no entregar) de una inferfaz de usuario para una aerolinea (por terminal...). 
+Esta aerolinea dispondrá de 10 vuelos para el dia de hoy, para empezar, estos vuelos estarán declarados de manera global, cuando se llame a la función:
+
+- Se preguntará por el nombre de usuario y dará la bienvenida.
+- El usuario visualizará todos los vuelos disponibles de una forma amigable:
+    El vuelo con origen: _Barcelona_, y destino: _Madrid_ tiene un coste de _XXXX€_ y no realiza _ninguna_ escala.
+- A continuación, el usuario verá el coste medio de los vuelos.
+- También podrá ver cuantos vuelos efectúan escalas.
+- Y, sabiendo que los ultimos 5 vuelos (los últimos 5 ID's) son los últimos del día, muestra al usuario sus destinos. */
