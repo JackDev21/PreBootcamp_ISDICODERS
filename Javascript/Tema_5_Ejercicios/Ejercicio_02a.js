2ae1fa0781509b32cfd148e3ccab364b464e5842
JackDev21 <jose.a.c.lopez@gmail.com>
2023-11-13 18:22:51 +0100

    Ejercicio 2.1 Tema 5 Ejercicios

--- Javascript/Tema_5_Ejercicios/Ejercicio_02a.js
+++ Javascript/Tema_5_Ejercicios/Ejercicio_02a.js
@@ -1,1 +1,20 @@
 // Puedes añadir además, la posición de cada resultado?
+
+
+function fibonacciSeries(fibonacciSequenceLength) { // Declaramos funcion fibonacciSeries con parametro fibonacciSequenceLength
+    const fibonacci = [{ // Declaramos array fibonacci con dos elementos ya definidos
+        posicion: 0, value: 0
+    }, {
+        posicion: 1, value: 1
+    }];
+
+    for (let i = 2; i < fibonacciSequenceLength; i++) { // Utilizamos bucle for para iterar mientras i sea menor que fibonacciSequenceLength, se inicia en 2 y se incrementa en 1 en cada iteración.
+        const siguienteNum = fibonacci[i - 1].value + fibonacci[i - 2].value; // Almacenamos en la variable siguienteNum la suma de los dos elementos anteriores. 
+        // Esto se hace porque la serie de Fibonacci comienza con 0 y 1, y cada término subsiguiente se calcula sumando los dos términos anteriores.
+        fibonacci.push({ posicion: i, value: siguienteNum });
+    }
+
+    return fibonacci;
+}
+
+console.log(fibonacciSeries(10));
--- Javascript/Tema_5_Ejercicios/Fibonacci.js
+++ Javascript/Tema_5_Ejercicios/Fibonacci.js
@@ -1,13 +1,14 @@
+const fibonacciSeries = (fibonacciSequenceLength) => {
+  // Declaramos funcion llamada fibonacciSeries, con un parámetro.
+  let n0 = 0; // Declaramos variable con valor 0, para primer elemento de la serie.
+  let n1 = 1; // Declaramos variable con valor 1, para segundo elemento de la serie, ya que necesita ser sumado con el anterior.
+  for (let i = 0; i < fibonacciSequenceLength; i++) {
+    // Bucle for que se ejecuta mientras i sea menor que fibonacciSequenceLength, se inicia en 0 y se incrementa en 1 en cada iteración.
+    console.log(n0); // Imprimimos el valor de n0.
+    let n2 = n0 + n1; // Se hace la suma y se almacena en la variable n2.
+    n0 = n1; // Se actualiza la variable n0 con el valor de n1.
+    n1 = n2; // Se actualiza la variable n1 con el valor de n2.
+  }
+};
 
-const fibonacciSeries = (fibonacciSequenceLength) => { // Declaramos funcion llamada fibonacciSeries, con un parámetro.
-    let n0 = 0; // Declaramos variable con valor 0, para primer elemento de la serie.
-    let n1 = 1; // Declaramos variable con valor 1, para segundo elemento de la serie, ya que necesita ser sumado con el anterior.
-    for (let i = 0; i < fibonacciSequenceLength; i++) { // Bucle for que se ejecuta mientras i sea menor que fibonacciSequenceLength, se inicia en 0 y se incrementa en 1 en cada iteración.
-        console.log(n0); // Imprimimos el valor de n0.
-        let n2 = n0 + n1; // Se hace la suma y se almacena en la variable n2.
-        n0 = n1; // Se actualiza la variable n0 con el valor de n1.
-        n1 = n2; // Se actualiza la variable n1 con el valor de n2.
-    }
-}
-
-fibonacciSeries(10)
+fibonacciSeries(10);
