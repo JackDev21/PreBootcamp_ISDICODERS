655d665adcfd7598f68ce095bc387e95db08dabf
JackDev21 <jose.a.c.lopez@gmail.com>
2023-11-27 11:09:38 +0100

    Tema 5 Ejercicios Ejercicio 6

--- Javascript_Tema_1-5/Tema_5_Ejercicios/Ejercicio_6.js
+++ Javascript_Tema_1-5/Tema_5_Ejercicios/Ejercicio_6.js
@@ -1,4 +1,38 @@
 // Crea un programa que use la encriptación Romana
 // 1 - Programa el desencriptador, pasa como parámetro CEORDS y que devuelva CODERS.
 
+/*¿Cómo es la encriptación Romana? Si tenemos la palabra CODERS, la palabra encriptada será CEORDS. Si divides la palabra original en 2 grupos obtienes:
+
+COD
+|-|-|
+ERS Entonces, uniendo las primeras letras de cada grupo, las segundas y las terceras obtienes CEORDS.
+Entonces, el programa deberá recibir CODERS y retornar CEORDS */
+
+
+
+const encryptRoma = (word) => {
+    const letterLength = word.length;
+    const pairLetter = [];
+    const oddLetter = [];
+
+    for (let index = 0; index < letterLength; index++) {
+        if (index % 2 === 0) {
+            pairLetter.push(word[index])
+        }
+        if (index % 2 !== 0) {
+            oddLetter.push(word[index])
+        }
+    }
+
+    return `${pairLetter.join("")}${oddLetter.join("")} `
+}
+
+
+
+const palabra = "LORENA"
+
+const programaEncriptado = encryptRoma(palabra)
+
+console.log(programaEncriptado)
+
 
