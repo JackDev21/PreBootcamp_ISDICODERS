6195a62c9792a32864957d4fc12ed4779c6f762f
JackDev21 <jose.a.c.lopez@gmail.com>
2023-11-30 11:36:00 +0100

    Ejercicio Calculadora Inicio

---
+++ Calculadora/Calculadora.js
@@ -0,0 +1,20 @@
+/*Calculator! ➗➕
+Haz una calculadora.
+
+Primera versión que no se entrega.
+Un único programa al que le pasarás dos argumentos que recogerás mediante el método prompt(); el usuario podrá visualizar por consola la suma, resta, multiplicación y 
+división entre ambos números. El resultado debería ser mostrado con 3 decimales como mucho (en caso de que hubieran). El programa debe gestionar y actuar correctamente 
+(gestión de errores) en el caso de que el usuario introduzca cualquier cosa que no sean números.
+
+Si el usuario introduce un solo numero, deberá mostrar SOLO su raíz cuadrada, si vuelve a introducir los dos, volverá a mostrar las 4 operaciones de siempre.
+Si el usuario introduce una letra, deberá mostrarle un aviso de que lo que ha introducido no es un número.
+Los resultados deberían almacenarse dentro de una array y mostrarlos de una forma amigable al usuario.
+Versión PRO para Entregar.
+Ahora que ya has hecho una primera versión de la calculadora. ¿Podrías hacer que la calculadora realice operaciones sean cuales sean el número de argumentos pasados a la función?
+Después de hacer todas las operaciones, el programa deberá preguntar al usuario si desea volver a realizar otra operación, volviendo a almacenar más resultados y mostrándolos.
+En ninguna de las dos versiones el usuario debe seleccionar la operación matemática, directamente se mostrarán las operaciones correspondientes (si hay un solo número se mostrará la raíz cuadrada de ese número y si hay más de un número se mostrarán las 4 operaciones).
+Prohibido usar la función eval().
+
+Documentación: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/rest_parameters */
+
+
