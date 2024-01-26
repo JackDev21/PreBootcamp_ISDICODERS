f21a027b3823026bbe4dace4d9bc16ffc060a19e
JackDev21 <jose.a.c.lopez@gmail.com>
2024-01-26 10:32:48 +0100

    check de si usuario no introduce nada

--- Proyectos_ejercicios/PasapalabraDos/main.js
+++ Proyectos_ejercicios/PasapalabraDos/main.js
@@ -2,182 +2,187 @@ const questions = [
     {
         letter: "a",
         answer: "Apu",
-        status: false,
         question: " Con la A: ¿Quién es el dueño de la tienda Kwik-E-Mart?",
+        status: 0,
     },
     {
         letter: "b",
         answer: "Bart",
-        status: false,
         question: "Con la B: ¿Cuál es el hijo mayor de la familia Simpson?",
+        status: 0,
     },
     {
         letter: "c",
         answer: "Central Nuclear",
-        status: false,
         question: "Con la C: ¿Donde trabaja Homer Simpson?",
+        status: 0,
     },
     {
         letter: "d",
         answer: "Duff",
-        status: false,
         question: "Con la D: ¿Cómo se llama la cerveza favorita en Springfield?",
+        status: 0,
     },
     {
         letter: "e",
         answer: "Edna",
-        status: false,
         question: "Con la E: ¿Quién es la maestra de Bart Simpson?",
+        status: 0,
     },
     {
         letter: "f",
         answer: "Flanders",
-        status: false,
         question: "Con la F: ¿Cómo se apellida el vecino de los Simpson?",
+        status: 0,
     },
     {
         letter: "g",
         answer: "Grafiti",
-        status: false,
         question: "Con la G: ¿En qué actividad creativa y a menudo problemática es conocido Bart Simpson?",
+        status: 0,
     },
     {
         letter: "h",
         answer: "Homer",
-        status: false,
         question: "Con la H: ¿Quién es el padre de la familia Simpson?",
+        status: 0,
     },
     {
         letter: "i",
         answer: "Itchy",
-        status: false,
         question: "Con la I: ¿Cómo se llama el ratón en el show de Krusty el payaso?",
+        status: 0,
     },
     {
         letter: "j",
         answer: "Jebediah Springfield",
-        status: false,
         question: "Con la J: ¿Quién es el fundador de Springfield?",
+        status: 0,
     },
     {
         letter: "k",
         answer: "Krusty",
-        status: false,
         question: "Con la K: ¿Cuál es el nombre del payaso en la televisión de Springfield?",
+        status: 0,
     },
     {
         letter: "l",
         answer: "Lisa Simpson",
-        status: false,
         question: "Con la L: ¿Cuál es el nombre de la hija mediana de la familia Simpson?",
+        status: 0,
     },
     {
         letter: "m",
         answer: "Maggie",
-        status: false,
         question: "Con la M: ¿Cómo se llama la hija más pequeña de la familia Simpson?",
+        status: 0,
     },
     {
         letter: "n",
         answer: "Ned Flanders",
-        status: false,
         question: "Con la N: ¿Cuál es el nombre del vecino religioso de los Simpson?",
+        status: 0,
     },
     {
         letter: "ñ",
         answer: "Ñu",
-        status: false,
         question: "Con la Ñ: ¿Cuál es el nombre del animal que Homer compra en un episodio para tener como mascota?",
+        status: 0,
     },
 
     {
         letter: "o",
         answer: "Otto",
-        status: false,
         question: "Con la O: ¿Cuál es el nombre del conductor del autobús escolar?",
+        status: 0,
     },
     {
         letter: "p",
         answer: "Patty",
-        status: false,
         question: "Con la P: ¿Cómo se llama la hermana gemela de Marge?",
+        status: 0,
     },
     {
         letter: "q",
         answer: "Quimby",
-        status: false,
         question: "Con la Q: ¿Cuál es el apellido del alcalde de Springfield?",
+        status: 0,
     },
     {
         letter: "r",
         answer: "Ralph",
-        status: false,
         question: "Con la R: ¿Cómo se llama el hijo torpe del Jefe Gorgory Wiggum?",
+        status: 0,
     },
     {
         letter: "s",
         answer: "Selma",
-        status: false,
         question: "Con la S: ¿Cómo se llama la hermana gemela de Marge?",
+        status: 0,
     },
     {
         letter: "t",
         answer: "Troy McClure",
-        status: false,
         question: "Con la T: ¿Cómo se llama el actor secundario en los Simpson?",
+        status: 0,
     },
     {
         letter: "u",
         answer: "Universidad",
-        status: false,
         question: "Con la U: ¿A qué lugar asiste Lisa cuando es admitida en la academia para niños superdotados?",
+        status: 0,
     },
     {
         letter: "v",
         answer: "Violín",
-        status: false,
         question: "Con la V: ¿Qué instrumento musical toca Lisa en varios episodios de Los Simpson?",
+        status: 0,
     },
 
     {
         letter: "w",
         answer: "Waylon Smithers",
-        status: false,
         question: "Con la W: ¿Cuál es el nombre del asistente personal de Mr. Burns?",
+        status: 0,
     },
     {
         letter: "x",
         answer: "Expediente X",
-        status: false,
         question: "Con la X: ¿Qué serie de televisión parodiaron en un episodio de Halloween?",
+        status: 0,
     },
     {
         letter: "y",
         answer: "Yeti",
-        status: false,
         question: "Con la Y: ¿Qué ser mitológico de pies grandes intenta encontrar Bart en el bosque?",
+        status: 0,
     },
     {
         letter: "z",
         answer: "Zia",
-        status: false,
         question: "Con la Z: ¿Cuál es el nombre de la hija de Lisa en el futuro?",
+        status: 0,
     },
 ];
 
-console.log(questions.length)
 
 
-let currentQuestionIndex = 0;
+let currentQuestionIndex = -1;
+let scoreValue = 27; // Define el valor inicial del puntaje fuera de la función
+let correctAnswer = 0;
+let incorrectAnswer = 0;
+let gameStarted = false; // Variable para verificar si el juego ya ha sido iniciado
+let crono;
+
+
 const showQuestion = () => {
-    if (currentQuestionIndex >= 0 && currentQuestionIndex < questions.length) {
-        const questionDisplay = document.querySelector('.question');
-        const questionText = questions[currentQuestionIndex].question;
-        questionDisplay.textContent = questionText;
 
+    if (currentQuestionIndex < questions.length) { // Verifica si hay preguntas restantes
+        const questionDisplay = document.querySelector('.question');
+        const questionText = questions[currentQuestionIndex].question; // Asigna el texto de la pregunta
+        questionDisplay.textContent = questionText; // Actualiza el texto de la pregunta
     }
 }
-
 
 const showResult = () => {
     const start = document.querySelector('#btn-start');
@@ -185,59 +190,74 @@ const showResult = () => {
 
     const result = document.querySelector('.result-container .result'); // Selecciona el elemento de resultado
     result.textContent = `Respuestas correctas: ${correctAnswer} Respuestas incorrectas: ${incorrectAnswer}`; // Actualiza el texto del elemento de resultado
-
 }
 
 
-let scoreValue = 27; // Define el valor inicial del puntaje fuera de la función
 
-let correctAnswer = 0;
-let incorrectAnswer = 0;
+const startGame = () => {
+    currentQuestionIndex++
+    if (!gameStarted) { // Verifica si el juego ya ha sido iniciado
+        gameStarted = true; // Marca el juego como iniciado
+        start.disabled = true; // Deshabilita el botón de inicio una vez que se inicia el juego
+        timeDisplay();
+        showQuestion();
+    }
+}
+const start = document.querySelector('#btn-start');
+start.addEventListener('click', startGame);
+
+
+
 const letter = document.querySelectorAll('.letter');
 
 const checkAnswer = () => {
-    const score = document.querySelector('.score'); // Selecciona el elemento de puntaje
-    const userAnswer = document.querySelector("#txtAnswer").value; // Obtén el valor del input
-    if (userAnswer.toLowerCase() === questions[currentQuestionIndex].answer.toLowerCase()) { // Comprueba si la respuesta es correcta
-        letter[currentQuestionIndex].classList.add("correct-answer") // Agrega la clase correct-answer al elemento correspondiente
-        scoreValue--; // Resta uno del puntaje si la respuesta es correcta
-        correctAnswer++; // Incrementa el contador de respuestas correctas
+    const score = document.querySelector('.score');
+    const userAnswer = document.querySelector("#txtAnswer").value;
+
+    if (userAnswer === "") {
+        return;
+    }
+
+    if (userAnswer.toLowerCase().trim() === questions[currentQuestionIndex].answer.toLowerCase().trim()) {
+        letter[currentQuestionIndex].classList.add("correct-answer");
+        correctAnswer++;
+        scoreValue--;
+        currentQuestionIndex++;
     } else {
         letter[currentQuestionIndex].classList.add("incorrect-answer");
-        incorrectAnswer++ // Incrementa el contador de respuestas incorrectas
+        incorrectAnswer++;
+        currentQuestionIndex++;
     }
-    currentQuestionIndex++; // Incrementa el índice de preguntas
-    showQuestion();
-    score.textContent = scoreValue; // Actualiza el contenido del elemento de puntaje
+
+    // Actualiza el contenido del elemento de puntaje
+    score.textContent = scoreValue;
 
     // Limpia el campo de respuesta después de comprobar
     document.querySelector("#txtAnswer").value = '';
+
+    // Muestra la siguiente pregunta
+    showQuestion();
 }
+
 // Asigna la función checkAnswer al evento click del botón
-document.querySelector('#btnsend').addEventListener('click', checkAnswer);
+const send = document.querySelector('#btnsend')
+send.addEventListener('click', checkAnswer);
 
 
-const pasarPalabra = (question) => {
-    if (currentQuestionIndex < questions.length) { // Si el índice es menor que el tamaño de la lista de preguntas seguimos 
-        letter[currentQuestionIndex].classList.add("pasapalabra-answer");
-        const wordPased = questions.splice(currentQuestionIndex - 1, 0)[0]; // Quita la palabra de la lista de preguntas y la asigna a la variable wordPased y elimina el elemento y resta -1 para ajustar el índice y no cambiar el orden
-        questions.push(wordPased); // Agrega la palabra a la última posición de la lista de preguntas (el último elemento del array)
-        currentQuestionIndex++
-        question.status = true // Actualiza el estado de la pregunta
-        showQuestion();
+const pasapalabra = () => {
+    letter[currentQuestionIndex].classList.add("pasapalabra-answer");
+    currentQuestionIndex++
 
-    }
-}
-// Asigna la función pasapalabra al evento click del botón
+};
 const pasapalabraButton = document.querySelector('#btnpasapalabra');
-pasapalabraButton.addEventListener('click', pasarPalabra);
+pasapalabraButton.addEventListener('click', pasapalabra);
 
 
 
-let crono;
 const timeDisplay = () => {
+
     const timer = document.querySelector('.timer');
-    let timeValue = 60;
+    let timeValue = 30;
 
     // Actualiza el tiempo cada segundo (1000 milisegundos)
     crono = setInterval(() => { // setInterval() devuelve un identificador del intervalo
@@ -246,10 +266,7 @@ const timeDisplay = () => {
 
         // Cuando el tiempo llegue a 0, limpia el intervalo para detener el contador
         if (timeValue < 0) {
-            showResult(); // Llama a la función showResult() para mostrar el resultado final
-            clearInterval(crono); // clearInterval() elimina el intervalo con el identificador dado
-            endGameDisplay.style.display = 'flex';
-            panelGame.style.visibility = 'hidden';
+            endGame();
         }
     }, 1000);
 };
@@ -258,25 +275,15 @@ const timeDisplay = () => {
 const endGameDisplay = document.querySelector('.end-game');
 const panelGame = document.querySelector('.panel-game');
 const close = document.querySelector('#btnclose');
+const closeGame = () => {
+    endGame();
+}
+close.addEventListener('click', closeGame);
+
 const endGame = () => {
     clearInterval(crono);
     showResult();
     endGameDisplay.style.display = 'flex';
     panelGame.style.visibility = 'hidden'; // El elemento se oculta pero mantiene su espacio.
-
 }
-close.addEventListener('click', endGame);
 
-
-let gameStarted = false; // Variable para verificar si el juego ya ha sido iniciado
-const start = document.querySelector('#btn-start');
-const startGame = () => {
-    if (!gameStarted) { // Verifica si el juego ya ha sido iniciado
-        gameStarted = true; // Marca el juego como iniciado
-        start.disabled = true; // Deshabilita el botón de inicio una vez que se inicia el juego
-        timeDisplay();
-        showQuestion();
-    }
-
-}
-start.addEventListener('click', startGame);
