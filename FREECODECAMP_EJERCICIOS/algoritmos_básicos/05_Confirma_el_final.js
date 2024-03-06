39c65f735fb83f0560301f7a2b195a956e0a2f1b
JackDev21 <jose.a.c.lopez@gmail.com>
2024-03-06 16:41:13 +0100

    Confirma el final

---
+++ FREECODECAMP_EJERCICIOS/algoritmos_básicos/05_Confirma_el_final.js
@@ -0,0 +1,15 @@
+/*Confirma el final
+
+Evalúa si una cadena (primer argumento, str) termina con la cadena de destino dada (segundo argumento, target). 
+Este desafío puede resolverse con el método .endsWith (),
+que fue introducido en ES2015. Pero para el propósito de este desafío,
+nos gustaría que usaras uno de los métodos de subcadena de JavaScript en su lugar. */
+
+function confirmEnding(str, target) {
+
+  let endOfStr = str.substring(str.length - target.length);
+
+  return endOfStr === target;
+}
+
+confirmEnding("Bastian", "n");
