d334e23e21565c0eafbd60867f353fd9f8f01d56
JackDev21 <jose.a.c.lopez@gmail.com>
2024-03-06 13:01:46 +0100

    Palabra mas larga

--- FREECODECAMP_EJERCICIOS/algoritmos_básicos/04_Palabra_mas_larga.js
+++ FREECODECAMP_EJERCICIOS/algoritmos_básicos/04_Palabra_mas_larga.js
@@ -4,7 +4,19 @@ Devuelve la longitud de la palabra más larga en la oración proporcionada.
 Tu respuesta debe ser un número. */
 
 function findLongestWordLength(str) {
-  return str.length;
+
+  str = str.split(" ");
+  console.log(str)
+
+  let max = 0;
+
+  for (let i = 0; i < str.length; i++) {
+
+    if (str[i].length > max) {
+      max = str[i].length
+    }
+  }
+  return max;
 }
 
 findLongestWordLength("The quick brown fox jumped over the lazy dog");
